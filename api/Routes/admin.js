f0fd21c36fb3ea24d39a259949f09c89be5325b1
Furkan Ersoz <furkanersoz1903@gmail.com>
2024-08-30 16:29:05 +0300

    added user update and some response fixes

--- api/Routes/admin.js
+++ api/Routes/admin.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getUsers, registerUser } = require("../database.js");
+const {
+  getUsers,
+  registerUser,
+  getUsersCount,
+  updateUser,
+} = require("../database.js");
 
 // "url/admin/<>"
 
@@ -8,12 +13,20 @@ router.get("/", (req, res) => {
   res.json({ message: "Merhaba Admin, hoş geldinnn!" });
 });
 
-router.get("/getUsers", (req, res) => {
-  return getUsers(req, res);
+router.get("/getUsers", async (req, res) => {
+  return await getUsers(req, res);
 });
 
-router.post("/registerUser", (req, res) => {
-  return registerUser(req, res);
+router.post("/registerUser", async (req, res) => {
+  return await registerUser(req, res);
+});
+
+router.get("/getUsersCount", async (req, res) => {
+  return await getUsersCount(req, res);
+});
+
+router.post("/updateUser", async (req, res) => {
+  return await updateUser(req, res);
 });
 
 module.exports = router;
